b301785b813ff965551cb61e75975333ccb00ccb
Matteo Zambon <matteo@thetophat.org>
2018-03-30 11:16:26 -0300

    Fabric forced to support JSDOM new API + version is now suffixed by tweak.0

--- dist/fabric.js
+++ dist/fabric.js
@@ -21,11 +21,7 @@ else {
     decodeURIComponent("%3C!DOCTYPE%20html%3E%3Chtml%3E%3Chead%3E%3C%2Fhead%3E%3Cbody%3E%3C%2Fbody%3E%3C%2Fhtml%3E")
   );
 
-  if (fabric.document.createWindow) {
-    fabric.window = fabric.document.createWindow();
-  } else {
-    fabric.window = fabric.document.parentWindow;
-  }
+  fabric.window = fabric.document.window;
 }
 
 /**
